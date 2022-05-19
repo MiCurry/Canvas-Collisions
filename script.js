29eda7fe72c421737576aa1f945b373ad4b771a6
Miles A. Curry <mcurry@cloudcampaign.com>
2022-05-19 06:51:40 -0600

    We need momentum

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 
+var animation;
+var isPaused = false;
+
+document.onkeypress = function(p) {
+    p = p || window.event;
+    pause();
+};
+
+function pause() {
+    if (!isPaused) {
+        clearInterval(animation)
+        isPaused = true;
+    }  else {
+        animation = setInterval(doAnim, time);
+        isPaused = false;
+    }
+}
+
 function dot(u, v) {
     return ((u.x - (u.x + u.dx)) * (v.x - (v.x + v.dx)) + (u.y - (u.y + u.dy)) * (v. y - (v.y + v.dy)));
 }
 
-function dot2(ux1, ux2, uy1, uy2, vx1, vx2, vy1, vy2) {
-    return (ux2 - ux1) * (vx2 - vx1) + (uy2 - vy1) * (vy2 - vy1);
+function dot2(u, v){
+    return u.dx * v.dx + u.dy * v.dy;
 }
 
-function vectorAngles(u, v) {
-    return Math.acos(dot(u, v) / (u.mag() * v.mag()));
-}
-
-function vectorAngles2(u, v, umag, vmag) {
-    console.log(u.x, u.dx, u.y, u.dy, v.x, v.dy, u.mag(), v.mag());
-    return Math.acos(dot2(u.x, u.x + u.dx, 
-	    		  u.y, u.y + u.dy, 
-	    	   	  v.x, v.x + v.dx,
-    			  v.y, v.y + v.dy) / (umag * vmag));
+function vectorAngles2(u, d, uMag, vMag) {
+    var dots = dot2(u,d,uMag,vMag);
+    var mags = uMag * vMag;
+    return  Math.acos(dot2(u, d) / (uMag * vMag));
 }
 
 function nearlyEqual(a, b, esp) {
@@ -57,8 +69,24 @@ class Object {
     collided(obj) {
         this.collisions.push(obj)
     }
+
+    hasCollided(obj) {
+        if (this.collisions.indexOf(obj) == -1) {
+            return false;
+        }
+        return true;
+    }
 }
 
+
+function redirect(v, theta) {
+        var dx = 0;
+        var dy = 0;
+        dx = v.dx * Math.cos(2*theta) + v.dy * Math.sin(2*theta);
+        dy = v.dx * Math.sin(2*theta) + - v.dy * -Math.cos(2*theta);
+        v.dx = dx;
+        v.dy = dy;
+    }
 
 class Circle extends Object {
     constructor(m, x, y, r, dx, dy, fill, canvas) {
@@ -70,11 +98,24 @@ class Circle extends Object {
 
     // Squared distance from an object
     squaredDistanceFrom(obj) {
-        return (this.x - obj.x) * (this.x - obj.x) + (this.y - obj.y) * (this.y - obj.y);
+        return (this.x - obj.x)**2  + (this.y - obj.y)**2 ;
+    }
+
+    xDistanceFrom(obj) {
+        return this.x - obj.x
+    }
+
+    yDistanceFrom(obj) {
+        return this.y - obj.y
     }
 
     incedentAngle(v) {
-    	return vectorAngles2(this, v, this.mag(), v.mag())
+        var dx = this.xDistanceFrom(v)
+        var dy = this.yDistanceFrom(v)
+        var distanceVector = { dx : dx,
+                               dy : dy,
+                               mag : Math.sqrt(dx**2 + dy**2) }
+        return vectorAngles2(this, distanceVector, this.mag(), distanceVector.mag)
     }
 
     doCollisions(objects) {
@@ -86,27 +127,24 @@ class Circle extends Object {
             let distanceSq = this.squaredDistanceFrom(objects[i]);
             let radiusSumSq = (this.r + objects[i].r) * (this.r + objects[i].r);
 
-	    if (distanceSq <= radiusSumSq) {
-
-                if (this.dx == 0 && this.dy == 0 || objects[i].dx == 0 && objects[i].dy == 0) {
-                    continue;
-                }
-
+	    if (distanceSq <= radiusSumSq + esp && !this.hasCollided(objects[i])) {
+                console.log("COLLISION");
                 // let theta = vectorAngles(this, objects[i]);
-		        let theta = this.incedentAngle(objects[i]);
+		        let theta = this.incedentAngle(objects[i], distanceSq);
+
+                console.log("Theta:", theta);
+                console.log("A1: ", this.dx, this.dy)
+                console.log("A2: ", objects[i].dx, objects[i].dy)
                 
+                redirect(this, theta);
+                redirect(objects[i], theta);
 
-                console.log("theta: ", theta, "cos(theta):", Math.cos(theta), Math.sin(theta), this.mag(), objects[i].mag());
+                console.log("B1: ", this.dx, this.dy)
+                console.log("B2: ", objects[i].dx, objects[i].dy)
 
-                this.dx = - this.dx * Math.cos(2*theta) + this.dy * Math.sin(2*theta);
-                this.dy = this.dx * Math.sin(2*theta) + this.dy * -Math.cos(2*theta);
-
-                console.log('b', this.dy, objects[i].dy);
-
-                objects[i].dx = - objects[i].dx * Math.cos(2*theta) + objects[i].dy * Math.sin(2*theta);
-                objects[i].dy = objects[i].dx * Math.sin(2*theta) + objects[i].dy * -Math.cos(2*theta);
-
-                console.log('a', this.dy, objects[i].dy);
+                this.collided(objects[i]);
+                objects[i].collided(this);
+                pause();
             } 
         }
     }
@@ -122,8 +160,10 @@ class Circle extends Object {
 
         this.x += this.dx;
         this.y += this.dy;
-    
-        this.collisions = [];
+
+    }
+
+    collide(){
         this.doCollisions(objects);
     }
 
@@ -144,14 +184,27 @@ class Circle extends Object {
 
 
 function doAnim() {
+    
     ctx = cvns.ctx;
     ctx.globalCompositeOperation = 'destination-over'; 
     cvns.clear();
 
     for (let i = 0; i < objects.length; i++) {
-        objects[i].draw(ctx);
-        objects[i].move()
+        objects[i].collisions = [];
     }
+
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].collide();
+    }
+
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].move();
+    }
+
+    for (let i = 0; i < objects.length; i++) {
+        objects[i].draw(ctx);
+    }
+
 }
 
 function randomCircle(cvns) {
@@ -171,12 +224,12 @@ function randomBalls(n) {
 
 function test2DHoriz() {
     objects.push(new Circle(1, 200, 250, 10, 5, 0, false, cvns));
-    objects.push(new Circle(1, 600, 250, 10, -5, 0, true, cvns));
+    objects.push(new Circle(1, 600, 250, 10, -5, 0, false, cvns));
 }
 
 function test2DVert() {
     objects.push(new Circle(1, 200, 150, 10, 0, 5, false, cvns));
-    objects.push(new Circle(1, 200, 250, 10, 0, -5, true, cvns));
+    objects.push(new Circle(1, 200, 250, 10, 0, -5, false, cvns));
 }
 
 function testGlanceVert() {
@@ -194,8 +247,18 @@ function diagnalTest() {
     objects.push(new Circle(1, 250, 250, 10, -1, -1, true, cvns));
 }
 
+function testGlanceDiag() {
+    objects.push(new Circle(1, 150, 150, 10, 10, 10, false, cvns));
+    objects.push(new Circle(1, 250, 260, 10, -10, -10, true, cvns));
+}
 
-var esp = 20;
+function sameDirection() {
+    objects.push(new Circle(1, 150, 150, 10, 5, 0, false, cvns));
+    objects.push(new Circle(1, 200, 150, 10, 1, 0, true, cvns));
+}
+
+
+var esp = 50;
 
 var maxDx = 10;
 var minDx = 1;
@@ -205,23 +268,30 @@ var maxR = 10;
 var minR = 20;
 var cvns;
 var objects = [];
+var x = 500;
+var y = 500;
+var time = 60;
 
 
 function draw() {
-    cvns = new Canvas(800, 500);
+    cvns = new Canvas(x, y);
 
-    //randomBalls(20);
+    //randomBalls(4);
+
+    sameDirection();
     
     //test2DHoriz();
     
-    test2DVert();
+    //test2DVert();
+
+    //diagnalTest();
+
+    //testGlanceDiag();
 
     //testGlanceVert();
 
     //testGlanceHoriz();
     
-    //diagnalTest();
 
-
-    setInterval(doAnim, 50)
+    animation = setInterval(doAnim, time)
 }
